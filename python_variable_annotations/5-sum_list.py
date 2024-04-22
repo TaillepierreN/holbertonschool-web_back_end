20f2a73f1b3dff74ba0f69430509ace2ba9741de
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-04-22 09:41:10 +0200

    add 5-sum_list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+"""
+type-annotated function which takes a list input_list of floats as argument
+and returns their sum as a float.
+"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """return sum of list of float"""
+    return (sum(input_list))
