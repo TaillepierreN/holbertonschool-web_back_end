3332052d3d9e8b4622aece37da06e70caf069a5f
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-04-22 11:48:23 +0200

    added 1-concurrent_coroutines

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-""" asynchronous coroutine"""
+"""
+asynchronous coroutine
+"""
 
 import asyncio
 import random
---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,19 @@
+"""
+async routine that takes in 2 int args n and max_delay
+spawn wait_random() <n> times with <max_delay>
+return list of all delays: float in ascending order
+"""
+
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+	"""async that wait <n> times and return the list of time waited"""
+	time_list = [wait_random(max_delay) for wait in range(n)]
+	delay_sorted_list = []
+	for time in asyncio.as_completed(time_list):
+		delay = await time
+		delay_sorted_list.append(delay)
+	return delay_sorted_list
