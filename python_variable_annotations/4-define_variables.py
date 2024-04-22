1ce6191b6c7893895c4201a806d40ac0c6691592
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-04-22 10:53:23 +0200

    4 remove extra space

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -8,7 +8,7 @@ i_understand_annotations, a boolean with a value of True
 school, a string with a value of “Holberton”
 """
 
-a:  int = 1
+a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
 school: str = "Holberton"
