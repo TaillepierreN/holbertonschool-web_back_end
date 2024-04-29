df8d8fd1106b80905990a3d52fd16e6662237af3
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-04-29 11:55:39 +0200

    change len(dataset)

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -57,7 +57,7 @@ class Server:
             total_pages: the total number of pages in the dataset as an integer
         """
         data = self.get_page(page, page_size)
-        total_items = len(self.dataset)
+        total_items = len(self.dataset())
         total_pages = math.ceil(total_items / page_size)
         next_page = page + 1 if page < total_pages else None
         prev_page = page - 1 if page > 1 else None
