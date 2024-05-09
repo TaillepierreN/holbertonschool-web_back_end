bdaf336e9028f7ce95bba90d95fdf465aec313ee
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-05-09 13:20:39 +0200

    rework 2, added check in constructor

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,5 +1,15 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!students.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students array must contain only strings');
+    }
+
     this._name = name;
     this._length = length;
     this._students = students;
@@ -10,7 +20,9 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
-    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = name;
   }
 
@@ -19,7 +31,9 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
-    if (typeof length !== 'number') throw TypeError('Length must be a number');
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
     this._length = length;
   }
 
